b622bbd63394e194a5a99e461cdce8e27382949b
Wilson Dev <hch.wilson@hynkor.com>
2024-10-13 00:59:33 -0500

    Creando componente de productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from '../../components/product/product.component';
-
+import { Product } from '../../../shared/models/product.model';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule,ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-fromChild(event:string){
-  console.log('estamos en el padre');
-  console.log(event);
-}
+  products = signal<Product[]>([])
+
+  constructor(){
+    const initProduct: Product[]=[
+      {
+        id: Math.random(),
+        title: 'Mouse teros negro',
+        price: 25,
+        image: 'https://picsum.photos/640/640?r=23',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Math.random(),
+        title: 'Disco SSD crucial MBX 240GB',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=24',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Math.random(),
+        title: 'Memoria RAM 8GB Fury',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=25',
+        createdAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProduct);
+  }
+  fromChild(event: string) {
+    console.log('estamos en el padre');
+    console.log(event);
+  }
 }
