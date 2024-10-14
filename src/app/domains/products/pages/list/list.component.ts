4d746fb7a67a47cfc5f7d3a161709f5161d0dd3b
Wilson Dev <hch.wilson@hynkor.com>
2024-10-13 22:04:51 -0500

    Obteniendo datos de una REST API

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,7 @@ import { ProductComponent } from '../../components/product/product.component';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { Product } from '../../../shared/models/product.model';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -15,67 +16,18 @@ import { CartService } from '../../../shared/services/cart.service';
 export class ListComponent {
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService)
 
-  constructor() {
-    const initProduct: Product[] = [
-      {
-        id: Math.random(),
-        title: 'Mouse teros negro',
-        price: 25,
-        image: 'https://picsum.photos/640/640?r=23',
-        createdAt: new Date().toISOString().slice(2, 10)
+  ngOnInit(){
+    this.productService.getProducts()
+    .subscribe({
+      next:(products)=>{
+        this.products.set(products);
       },
-      {
-        id: Math.random(),
-        title: 'Disco SSD crucial MBX 240GB',
-        price: 150,
-        image: 'https://picsum.photos/640/640?r=24',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Memoria RAM 8GB Fury',
-        price: 150,
-        image: 'https://picsum.photos/640/640?r=25',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Laptop Lenovo Idea pad',
-        price: 1500,
-        image: 'https://picsum.photos/640/640?r=26',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Impresora Epson L3290',
-        price: 960,
-        image: 'https://picsum.photos/640/640?r=27',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Laptop HP 250 ',
-        price: 1200,
-        image: 'https://picsum.photos/640/640?r=28',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Memoria USB 23GB HP',
-        price: 35,
-        image: 'https://picsum.photos/640/640?r=29',
-        createdAt: new Date().toISOString().slice(2, 10)
-      },
-      {
-        id: Math.random(),
-        title: 'Case termaltake negro',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=30',
-        createdAt: new Date().toISOString().slice(2, 10)
+      error:()=>{
+
       }
-    ];
-    this.products.set(initProduct);
+    })
   }
 
   addToCart(product: Product) {
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -2,6 +2,7 @@ export interface Product {
   id: number;
   title: string;
   price: number;
-  image: string;
-  createdAt: string;
+  description: string;
+  category: string[];
+  images: string[];
 }
