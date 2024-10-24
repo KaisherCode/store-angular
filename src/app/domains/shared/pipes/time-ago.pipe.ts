fc6de841f037319ea333bd25372bc185debee170
Wilson Dev <hch.wilson@hynkor.com>
2024-10-23 23:11:59 -0500

    fix date js

--- src/app/domains/shared/pipes/time-ago.pipe.ts
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {formatDistance} from 'date-fns';
+import { formatDistance } from 'date-fns';
 
 @Pipe({
   name: 'timeAgo',
@@ -10,7 +10,7 @@ export class TimeAgoPipe implements PipeTransform {
   transform(value: string): string {
     const date = new Date(value);
     const today = new Date();
-    return formatDistance(today,date);
+    return formatDistance(today, date);
   }
 
 }
